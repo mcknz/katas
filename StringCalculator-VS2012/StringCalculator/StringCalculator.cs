a6c1146866c14bd0e0f8aecd4d6eaf5b6c1099e4
Jeff McKenzie <mail@mcknz.com>
2013-07-11 14:59:19 -0400

    added string calculator kata implementation in C# in VS2012

---
+++ StringCalculator-VS2012/StringCalculator/StringCalculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private bool _hasBracketedCustomDelimiter;
+        private bool _hasCustomDelimiter;
+        private int _lastBracketPosition;
+
+        public int Add(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+
+            _hasBracketedCustomDelimiter = input.StartsWith("//[");
+            _hasCustomDelimiter = input.StartsWith("//") && !_hasBracketedCustomDelimiter;
+            _lastBracketPosition = input.LastIndexOf("]");
+
+            return AddStringNumbers(
+                GetNumbers(input),
+                GetDelimiters(input)
+            );
+        }
+
+        private int AddStringNumbers(string nums, string[] delimiters)
+        {
+            int total = 0;
+
+            nums.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(n => total += int.Parse(n) > 1000 ? 0 : int.Parse(n));
+
+            return total;
+        }
+
+        private string[] GetDelimiters(string input)
+        {
+            var delimiters = new List<string>() { ",","\n" };
+
+            if (_hasCustomDelimiter)
+            {
+                delimiters.Add(input.Substring(2, 1));
+            }
+            else
+            {
+                if (_hasBracketedCustomDelimiter)
+                {
+                    input
+                        .Substring(2, _lastBracketPosition - 1)
+                        .Replace("][", ",")
+                        .Replace("[", "")
+                        .Replace("]", "")
+                        .Split(',')
+                        .ToList()
+                        .ForEach(n => delimiters.Add(n));
+                }
+            }
+            
+            return delimiters.ToArray();
+        }
+
+        private string GetNumbers(string input)
+        {
+            if (!_hasBracketedCustomDelimiter && !_hasCustomDelimiter)
+            {
+                return input;
+            }
+
+            return _hasBracketedCustomDelimiter
+                ? input.Substring(_lastBracketPosition + 1)
+                : input.Substring(4);                
+        }
+    }
+}
---
+++ StringCalculator-VS2012/StringCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestEmptyStringReturnsZero()
+        {
+            //Assert
+            Assert.AreEqual(0, GetAddResult(""));
+        }
+
+        [TestMethod]
+        public void TestOneStringNumberReturnsNumberOne()
+        {
+            Assert.AreEqual(1, GetAddResult("1"));
+        }
+
+        [TestMethod]
+        public void TestTwoNumbersWithCommaReturnsSum()
+        {
+            Assert.AreEqual(3, GetAddResult("1,2"));
+        }
+
+        [TestMethod]
+        public void TestManyNumbersWithCommaReturnsSum()
+        {
+            Assert.AreEqual(21, GetAddResult("1,2,4,5,9,0"));
+        }
+
+        [TestMethod]
+        public void TestNumbersWithNewLineAndCommaReturnsSum()
+        {
+            Assert.AreEqual(21, GetAddResult("1\n2,4,5,9,0"));
+        }
+
+        [TestMethod]
+        public void TestNumbersWithCustomDelimiter()
+        {
+            Assert.AreEqual(3, GetAddResult("//;\n1;2"));
+        }
+
+        [TestMethod]
+        public void TestNumbersOver1000DontCount()
+        {
+            Assert.AreEqual(2, GetAddResult("1001,2"));
+        }
+
+        [TestMethod]
+        public void TestMultiLengthDelimiter()
+        {
+            Assert.AreEqual(6, GetAddResult("//[***]\n1***2***3"));
+        }
+
+        [TestMethod]
+        public void TestMultipleCustomDelimiter()
+        {
+            Assert.AreEqual(7, GetAddResult("//[***][^^]\n1***2***3^^1"));
+        }
+
+        [TestMethod]
+        public void TestMultipleCustomDelimiter2()
+        {
+            Assert.AreEqual(6, GetAddResult("//[*][%]\n1*2%3"));
+        }        
+
+        private int GetAddResult(string input)
+        {
+            return new Calculator().Add(input);            
+        }
+    }
+}
